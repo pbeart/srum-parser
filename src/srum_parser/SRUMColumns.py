d0e2a7529095f9feef61cced46c45521c78f266b
Kristian Nymann Jakobsen <kristian@nymann.dev>
2020-06-08 02:17:22 +0200

    [WIP]: Changed structure

--- src/srum_parser/SRUMColumns.py
+++ src/srum_parser/SRUMColumns.py
@@ -1,15 +1,16 @@
 "Provide functions to handle processing of certain columns from SRUM tables into a more useful format"
 
-def parse_notification_type(type_id): 
+
+def parse_notification_type(type_id):
     """https://docs.microsoft.com/en-us/uwp/api/windows.networking.pushnotifications.pushnotificationtype?view=winrt-18362
     Returns a named notification type from a notification type ID"""
 
     notification_types = {
-    0: "Toast",
-    1: "Tile",
-    2: "Badge",
-    3: "Raw",
-    4: "TileFlyout"
+        0: "Toast",
+        1: "Tile",
+        2: "Badge",
+        3: "Raw",
+        4: "TileFlyout"
     }
 
     if type_id in notification_types:
--- src/tests/test_ESEUtils.py
+++ src/tests/test_ESEUtils.py
@@ -2,7 +2,9 @@ import ESEUtils
 import pytest
 import datetime
 
+
 class TestParseEseValues:
+
     def test_nil(self):
         assert ESEUtils.parse_ese_value(b"teststring", 0) == None
 
@@ -17,7 +19,7 @@ class TestParseEseValues:
     def test_signed_short(self):
         assert ESEUtils.parse_ese_value(b"\xff\xff", 3) == -1
         assert ESEUtils.parse_ese_value(b"\x45\x03", 3) == 0x0345
-    
+
     def test_signed_int(self):
         assert ESEUtils.parse_ese_value(b"\xff\xff\xff\xff", 4) == -1
         assert ESEUtils.parse_ese_value(b"\x01\x45\x03\x00", 4) == 0x034501
@@ -27,42 +29,58 @@ class TestParseEseValues:
         assert ESEUtils.parse_ese_value(b'@@QH', 6) == 0x034501
 
     def test_double(self):
-        assert ESEUtils.parse_ese_value(b'\x00\x00\x00\x00\x00\x00\xf0\xbf', 7) == -1
-        assert ESEUtils.parse_ese_value(b"\x00\x00\x00\x00\x08\x28\x0a\x41", 7) == 0x034501
-    
+        assert ESEUtils.parse_ese_value(b'\x00\x00\x00\x00\x00\x00\xf0\xbf',
+                                        7) == -1
+        assert ESEUtils.parse_ese_value(b"\x00\x00\x00\x00\x08\x28\x0a\x41",
+                                        7) == 0x034501
+
     def test_datetime(self):
         # FILETIME:
         target_date = datetime.datetime(2010, 6, 29, 9, 47, 42, 754212)
-        parsed_date = ESEUtils.parse_ese_value(b'\x01\xcb\x17\x70\x1e\x9c\x88\x5a'[::-1], 8)
+        parsed_date = ESEUtils.parse_ese_value(
+            b'\x01\xcb\x17\x70\x1e\x9c\x88\x5a'[::-1], 8)
         assert abs((target_date - parsed_date).total_seconds()) < 1
 
         # OLE Time:
-        target_date = datetime.datetime(2019, 12, 22, 13, 18,)
+        target_date = datetime.datetime(
+            2019,
+            12,
+            22,
+            13,
+            18,
+        )
         parsed_date = ESEUtils.parse_ese_value(b'\xbc\xbb\xbb\xbbqe\xe5@', 8)
         assert abs((target_date - parsed_date).total_seconds()) < 1
 
     def test_binary_largebinary(self):
-        assert ESEUtils.parse_ese_value(b'\xde\xad\xbe\xef', 9) == repr(b'\xde\xad\xbe\xef') # binary
-        assert ESEUtils.parse_ese_value(b'\xde\xad\xbe\xef', 11) == repr(b'\xde\xad\xbe\xef') # large binary
+        assert ESEUtils.parse_ese_value(b'\xde\xad\xbe\xef', 9) == repr(
+            b'\xde\xad\xbe\xef')  # binary
+        assert ESEUtils.parse_ese_value(b'\xde\xad\xbe\xef', 11) == repr(
+            b'\xde\xad\xbe\xef')  # large binary
 
     def test_text_largetext(self):
-        assert ESEUtils.parse_ese_value(b'ham sandwiches', 10) == "ham sandwiches" # text
-        assert ESEUtils.parse_ese_value(b'sam handwiches', 12) == "sam handwiches" # large text
+        assert ESEUtils.parse_ese_value(b'ham sandwiches',
+                                        10) == "ham sandwiches"  # text
+        assert ESEUtils.parse_ese_value(b'sam handwiches',
+                                        12) == "sam handwiches"  # large text
 
     def test_unsigned_int(self):
         assert ESEUtils.parse_ese_value(b"\xff\xff\xff\xff", 14) == 4294967295
         assert ESEUtils.parse_ese_value(b"\x01\x45\x03\x00", 14) == 0x034501
 
     def test_signed_long_long(self):
-        assert ESEUtils.parse_ese_value(b"\xff\xff\xff\xff\xff\xff\xff\xff", 15) == -1
-        assert ESEUtils.parse_ese_value(b"\x01\x45\x03\x00\x00\x00\x00\x00", 15) == 0x034501
-    
+        assert ESEUtils.parse_ese_value(b"\xff\xff\xff\xff\xff\xff\xff\xff",
+                                        15) == -1
+        assert ESEUtils.parse_ese_value(b"\x01\x45\x03\x00\x00\x00\x00\x00",
+                                        15) == 0x034501
+
     def test_guid(self):
         raw = b"\xde\xad\xbe\xef\xfe\xed\xbe\xee\xde\xad\xbe\xef\xfe\xed\xbe\xee"
         expected = "{deadbeef-feed-be-ee-deadbeeffeedbeee}"
         assert ESEUtils.parse_ese_value(raw, 16) == expected
 
     def test_unsigned_short(self):
-        assert ESEUtils.parse_ese_value(b"\xff\xff\xff\xff\xff\xff\xff\xff", 15) == -1
-        assert ESEUtils.parse_ese_value(b"\x01\x45\x03\x00\x00\x00\x00\x00", 15) == 0x034501
-    
+        assert ESEUtils.parse_ese_value(b"\xff\xff\xff\xff\xff\xff\xff\xff",
+                                        15) == -1
+        assert ESEUtils.parse_ese_value(b"\x01\x45\x03\x00\x00\x00\x00\x00",
+                                        15) == 0x034501
